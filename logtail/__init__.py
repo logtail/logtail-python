f0369077a21cd1d411bb73541a69fc234ac246da
Simon Rozsival <simon@rozsival.com>
2021-03-25 16:29:39 +0100

    :construction: Bump version number

--- logtail/__init__.py
+++ logtail/__init__.py
@@ -5,6 +5,6 @@ from .handler import LogtailHandler
 from .helpers import LogtailContext, DEFAULT_CONTEXT
 from .formatter import LogtailFormatter
 
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 
 context = DEFAULT_CONTEXT
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 from setuptools import setup
 
 
-VERSION = '0.1.0'
+VERSION = '0.1.1'
 ROOT_DIR = os.path.dirname(__file__)
 
 REQUIREMENTS = [

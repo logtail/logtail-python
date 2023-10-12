25c6787f2d13a0224b52ca0eacca7f47778bc77b
Petr Heinz <petr@betterstack.com>
2023-10-12 18:04:58 +0200

    bump version to 0.2.7

--- logtail/__init__.py
+++ logtail/__init__.py
@@ -5,6 +5,6 @@ from .handler import LogtailHandler
 from .helpers import LogtailContext, DEFAULT_CONTEXT
 from .formatter import LogtailFormatter
 
-__version__ = '0.2.6'
+__version__ = '0.2.7'
 
 context = DEFAULT_CONTEXT
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 from setuptools import setup
 
 
-VERSION = '0.2.6'
+VERSION = '0.2.7'
 ROOT_DIR = os.path.dirname(__file__)
 
 REQUIREMENTS = [

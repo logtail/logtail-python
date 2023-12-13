998b8adecace88e784613cbf5e2dabc5ca4c9f6d
Petr Heinz <petr@betterstack.com>
2023-12-13 18:19:22 +0100

    Remove now redundant test cases in TestLogtailFormatter

--- tests/test_formatter.py
+++ tests/test_formatter.py
@@ -61,59 +61,6 @@ class TestLogtailFormatter(unittest.TestCase):
         self.assertEqual(frame['order'], self.order)
         self.assertEqual(frame['context']['customer'], {'id': self.customer['id'], 'trusted': True})
 
-    def test_format_with_custom_default_json_serializer(self):
-        def suppress_encoding_errors(obj):
-            return 'Could not encode type=%s' % type(obj).__name__
-
-        default_formatter = logtail.LogtailFormatter(context=self.context)
-        default_logger, _ = logger_and_lines(default_formatter, 'default')
-
-        suppress_formatter = logtail.LogtailFormatter(context=self.context, json_default=suppress_encoding_errors)
-        suppress_logger, loglines = logger_and_lines(suppress_formatter, 'suppress')
-
-        self.assertIsNot(default_logger, suppress_logger)
-
-        with self.context(data={'not_encodable': Dummy()}):
-            with self.assertRaises(TypeError):
-                default_logger.info('hello')
-            suppress_logger.info('goodbye')
-
-        line = self._check_and_get_line(loglines)
-        frame = json.loads(line)
-        self.assertEqual(frame['message'], 'goodbye')
-        self.assertEqual(frame['context']['data'], {'not_encodable': 'Could not encode type=Dummy'})
-
-    def test_format_with_custom_default_json_encoder(self):
-        default_formatter = logtail.LogtailFormatter(context=self.context)
-        default_logger, _ = logger_and_lines(default_formatter, 'default')
-
-        dummy_capable_formatter = logtail.LogtailFormatter(context=self.context, json_encoder=DummyCapableEncoder)
-        dummy_capable_logger, loglines = logger_and_lines(dummy_capable_formatter, 'dummy_capable')
-
-        self.assertIsNot(default_logger, dummy_capable_logger)
-
-        with self.context(data={'not_encodable': Dummy()}):
-            with self.assertRaises(TypeError):
-                default_logger.info('hello')
-            dummy_capable_logger.info('goodbye')
-
-        line = self._check_and_get_line(loglines)
-        frame = json.loads(line)
-        self.assertEqual(frame['message'], 'goodbye')
-        self.assertEqual(frame['context']['data'], {'not_encodable': '<Dummy instance>'})
-
-
-class Dummy(object):
-    """ Because this is a custom class, it cannot be encoded by the default JSONEncoder. """
-
-
-class DummyCapableEncoder(json.JSONEncoder):
-    """ A JSONEncoder that can encode instances of the Dummy class. """
-    def default(self, obj):
-        if isinstance(obj, Dummy):
-            return '<Dummy instance>'
-        return super(CustomEncoder, self).default(obj)
-
 
 class ListHandler(logging.Handler):
     """ Accumulates all log lines in a list for testing purposes. """
